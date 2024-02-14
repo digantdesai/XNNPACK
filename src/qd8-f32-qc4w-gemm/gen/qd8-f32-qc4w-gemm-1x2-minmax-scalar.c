5a138fbf68906e594046904c8e3ec6f23a5deeb9
Digant Desai <digantdesai@meta.com>
2024-02-14 09:48:15 -0600

    remove debug from 1x2 scalar kernels

--- src/qd8-f32-qc4w-gemm/gen/qd8-f32-qc4w-gemm-1x2-minmax-scalar.c
+++ src/qd8-f32-qc4w-gemm/gen/qd8-f32-qc4w-gemm-1x2-minmax-scalar.c
@@ -126,8 +126,6 @@ void xnn_qd8_f32_qc4w_bl_gemm_minmax_ukernel_1x2__scalar(
     const union xnn_f32_qc4w_minmax_params params[restrict XNN_MIN_ELEMENTS(1)],
     const struct xnn_qd8_quantization_params quantization_params[restrict XNN_MIN_ELEMENTS(1)])
 {
-  printf("Running kernel %s: mr=%zu, nc=%zu, kc=%zu, bl=%zu\n", __func__, mr, nc, kc, bl);
-
   assert(mr != 0);
   assert(mr <= 1);
   assert(nc != 0);
@@ -138,90 +136,66 @@ void xnn_qd8_f32_qc4w_bl_gemm_minmax_ukernel_1x2__scalar(
   size_t nc_block = 0;
 
   kc = round_up_po2(kc, 2);
-  printf("___ kernel ___");
   do {
-    printf("\nnc_block=%zu, w: %p\n", nc_block++, w);
     const float vksum0 = unaligned_indexed_load_f32(w, 0);
     const float vksum1 = unaligned_indexed_load_f32(w, 1);
-    printf("kernel: kfsum: 0: %f, 1: %f\n", vksum0, vksum1);
     w = (const float*) w + 2;
+
     const int32_t vinput_zero_point0 = quantization_params[0].zero_point;
 
-    float vout0x0 = 0.0f;
-    float vout0x1 = 0.0f;
-
-    printf("out0: %f, out1: %f\n", vout0x0, vout0x1);
-    printf("izp0: %d, vksum0: %f, vksum0 * izp0: %f\n", vinput_zero_point0, vksum0, (float) vinput_zero_point0 * vksum0);
-    printf("izp0: %d, vksum1: %f, vksum1 * izp0: %f\n", vinput_zero_point0, vksum1, (float) vinput_zero_point0 * vksum1);
-    vout0x0 += (float) vinput_zero_point0 * vksum0;
-    vout0x1 += (float) vinput_zero_point0 * vksum1;
+    float vout0x0 = (float) vinput_zero_point0 * vksum0;
+    float vout0x1 = (float) vinput_zero_point0 * vksum1;
 
     size_t n_blocks = kc / bl;
 
     for (size_t nb=0; nb<n_blocks; ++nb){
       int32_t vacc0x0 = 0;
       int32_t vacc0x1 = 0;
-      printf("\nnb=%zu, w: %p\n", nb, w);
       for (size_t k=bl; k >= 2 * sizeof(uint8_t); k -= 2 * sizeof(uint8_t)) {
-        printf("k=%zu, w: %p\n", k, w);
         const int32_t va0c0 = (int32_t) a0[0];
         const int32_t va0c1 = (int32_t) a0[1];
         a0 += 2;
-        printf("va0c0: %d, va0c1: %d\n", va0c0, va0c1);
 
         const uint8_t vbi0 = ((const uint8_t*) w)[0];
         const uint8_t vbi1 = ((const uint8_t*) w)[1];
-        printf("w:%p, vbi0=%4d, vbi1=%4d, ", (void*)w, (int32_t)vbi0, (int32_t)vbi1);
         w = (const uint8_t*) w + 2;
 
         const int32_t vb0c0 = (int32_t) (int8_t) (vbi0 << 4);
         const int32_t vb0c1 = (int32_t) (int8_t) (vbi0 & 0xF0);
         const int32_t vb1c0 = (int32_t) (int8_t) (vbi1 << 4);
         const int32_t vb1c1 = (int32_t) (int8_t) (vbi1 & 0xF0);
-        printf("vb0c0=%4d, vb0c1=%4d, vb1c0=%4d, vb1c1=%4d\n", vb0c0, vb0c1, vb1c0, vb1c1);
 
         vacc0x0 += va0c0 * vb0c0;
         vacc0x1 += va0c0 * vb1c0;
         vacc0x0 += va0c1 * vb0c1;
         vacc0x1 += va0c1 * vb1c1;
       }
-      printf("int acc0x0: %d, acc0x1: %d\n", vacc0x0, vacc0x1);
       float vf0x0 = vacc0x0;
       float vf0x1 = vacc0x1;
 
       const float vfilter_output_scale0 = unaligned_indexed_load_f32(w, 0);
-      printf("w: %p, scale0=%4.2f\n", (void*)w, vfilter_output_scale0);
       w = (const float*) w + 1;
       vf0x0 *= vfilter_output_scale0;
 
       const float vfilter_output_scale1 = unaligned_indexed_load_f32(w, 0);
-      printf("w: %p, scale1=%4.2f\n", (void*)w, vfilter_output_scale1);
       w = (const float*) w + 1;
       vf0x1 *= vfilter_output_scale1;
 
       vout0x0 += vf0x0;
       vout0x1 += vf0x1;
-
-      printf("acc0: %d, acc1: %d\n", vacc0x0, vacc0x1);
-      vacc0x0 = 0;
-      vacc0x1 = 0;
     }
 
-    printf("out0: %f, out1: %f\n", vout0x0, vout0x1);
     vout0x0 /= 16;
     vout0x1 /= 16;
-    printf("out0: %f, out1: %f\n", vout0x0, vout0x1);
 
     const float vinput_scale0 = quantization_params[0].inv_scale;
     vout0x0 *= vinput_scale0;
     vout0x1 *= vinput_scale0;
-    printf("out0: %f, out1: %f, inv_scale: %f\n", vout0x0, vout0x1, vinput_scale0);
 
     const float vbias0 = unaligned_indexed_load_f32(w, 0);
     vout0x0 += vbias0;
     const float vbias1 = unaligned_indexed_load_f32(w, 1);
     vout0x1 += vbias1;
-    printf("w: %p, bias0: %f, bias1: %f\n", w, vbias0, vbias1);
     w = (const float*) w + 2;
 
     const float voutput_min = params->scalar.min;
--- src/qd8-f32-qc8w-gemm/gen/qd8-f32-qc8w-gemm-1x2-minmax-scalar.c
+++ src/qd8-f32-qc8w-gemm/gen/qd8-f32-qc8w-gemm-1x2-minmax-scalar.c
@@ -29,7 +29,6 @@ void xnn_qd8_f32_qc8w_gemm_minmax_ukernel_1x2__scalar(
     const union xnn_f32_minmax_params params[restrict XNN_MIN_ELEMENTS(1)],
     const struct xnn_qd8_quantization_params quantization_params[restrict XNN_MIN_ELEMENTS(1)])
 {
-  printf("Running kernel %s\n", __func__);
   assert(mr != 0);
   assert(mr <= 1);
   assert(nc != 0);
@@ -120,11 +119,6 @@ void xnn_qd8_f32_qc8w_bl_gemm_minmax_ukernel_1x2__scalar(
     const union xnn_f32_minmax_params params[restrict XNN_MIN_ELEMENTS(1)],
     const struct xnn_qd8_quantization_params quantization_params[restrict XNN_MIN_ELEMENTS(1)])
 {
-  // xnn_qd8_f32_qc8w_gemm_minmax_ukernel_1x2__scalar(mr, nc, kc, a, a_stride, w, c, cm_stride, cn_stride, params, quantization_params);
-  // return;
-
-  printf("Running kernel %s: mr=%zu, nc=%zu, kc=%zu, bl=%zu\n", __func__, mr, nc, kc, bl);
-
   assert(mr != 0);
   assert(mr <= 1);
   assert(nc != 0);
@@ -135,60 +129,44 @@ void xnn_qd8_f32_qc8w_bl_gemm_minmax_ukernel_1x2__scalar(
 
   size_t nc_block = 0;
   do {
-    printf("\nnc_block=%zu, w: %p\n", nc_block++, w);
     const float vksum0 = unaligned_indexed_load_f32(w, 0);
     const float vksum1 = unaligned_indexed_load_f32(w, 1);
-    printf("kernel: kfsum: 0: %f, 1: %f\n", vksum0, vksum1);
     w = (const int32_t*) w + 2;
+
     const int32_t vinput_zero_point0 = quantization_params[0].zero_point;
 
-    float vout0x0 = 0.0f;
-    float vout0x1 = 0.0f;
+    float vout0x0 = (float) vinput_zero_point0 * vksum0;
+    float vout0x1 = (float) vinput_zero_point0 * vksum1;
 
     size_t n_blocks = kc / bl;
 
     for(size_t nb=0; nb<n_blocks; ++nb) {
       int32_t vacc0x0 = 0;
       int32_t vacc0x1 = 0;
-      printf("\nnb=%zu, w: %p\n", nb, w);
       for(size_t k=0; k < bl; k += sizeof(int8_t)) {
-        printf("k=%zu, w: %p\n", k, w);
         const int32_t va0 = (int32_t) *a0++;
-        printf("a0:%p, va0=%4d\n", (int8_t*)a0 - 1, va0);
 
         const int32_t vb0 = (int32_t) ((const int8_t*) w)[0];
         const int32_t vb1 = (int32_t) ((const int8_t*) w)[1];
-        printf("w:%p, vb0=%4d, vb1=%4d\n", (void*)w, vb0, vb1);
         w = (const int8_t*) w + 2;
 
         vacc0x0 += va0 * vb0;
         vacc0x1 += va0 * vb1;
       }
-      printf("int acc0x0: %d, acc0x1: %d\n", vacc0x0, vacc0x1);
       float vf0x0 = (float) vacc0x0;
       float vf0x1 = (float) vacc0x1;
 
       const float vfilter_output_scale0 = unaligned_indexed_load_f32(w, 0);
-      printf("w: %p, scale0=%4.2f\n", (void*)w, vfilter_output_scale0);
       w = (const float*) w + 1;
       vf0x0 *= vfilter_output_scale0;
 
       const float vfilter_output_scale1 = unaligned_indexed_load_f32(w, 0);
-      printf("w: %p, scale1=%4.2f\n", (void*)w, vfilter_output_scale1);
       w = (const float*) w + 1;
       vf0x1 *= vfilter_output_scale1;
 
       vout0x0 += vf0x0;
       vout0x1 += vf0x1;
-
-      printf("acc0: %d, acc1: %d\n", vacc0x0, vacc0x1);
-      vacc0x0 = 0;
-      vacc0x1 = 0;
     }
-
-    printf("vksum * izp: %f\n", (float) vinput_zero_point0 * vksum0);
-    vout0x0 += (float) vinput_zero_point0 * vksum0;
-    vout0x1 += (float) vinput_zero_point0 * vksum1;
 
     const float vinput_scale0 = quantization_params[0].inv_scale;
     vout0x0 *= vinput_scale0;
@@ -198,8 +176,6 @@ void xnn_qd8_f32_qc8w_bl_gemm_minmax_ukernel_1x2__scalar(
     vout0x0 += vbias0;
     const float vbias1 = unaligned_indexed_load_f32(w, 1);
     vout0x1 += vbias1;
-
-    printf("w: %p, bias0=%4.2f, bias1:%4.2f\n", (void*)w, vbias0, vbias1);
     w = (const float*) w + 2;
 
     const float voutput_min = params->scalar.min;
